496684b25776a4e1b1a385aeda41e70fd4c87fae
Garrett Hays <103149986+GarrettHays@users.noreply.github.com>
2022-09-28 19:24:34 -0700

    adds mock-entries file

---
+++ src/app/mock-entries.ts
@@ -0,0 +1,13 @@
+import { Entry } from './entry';
+
+export const ENTRIES: Entry[] = [
+  { id: 2, name: 'Rockport, ME', date: '05/01/2020' },
+  { id: 3, name: 'North Hampton, NH', date: '06/01/2020' },
+  { id: 4, name: 'Sparta, MI', date: '07/01/2020'},
+  { id: 5, name: 'Enochville, NC', date: '08/01/2020'},
+  { id: 6, name: 'Thomaston, CT', date: '09/01/2020'},
+  { id: 7, name: 'Hazel Green, WI', date: '10/01/2020'},
+  { id: 8, name: 'Douglass, KS', date: '11/01/2020'},
+  { id: 9, name: 'Aledo, TX', date: '12/01/2020'},
+  { id: 10, name: 'Summit, NJ', date: '01/01/2021'}
+];
