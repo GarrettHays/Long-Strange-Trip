e02b0b1fa3036dcf1fef7fe2f949a2aea169cfa4
Garrett Hays <103149986+GarrettHays@users.noreply.github.com>
2022-09-28 19:24:58 -0700

    adds entries details component files

---
+++ src/app/entries-detail/entries-detail.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EntriesDetailComponent } from './entries-detail.component';
+
+describe('EntriesDetailComponent', () => {
+  let component: EntriesDetailComponent;
+  let fixture: ComponentFixture<EntriesDetailComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ EntriesDetailComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(EntriesDetailComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/entries-detail/entries-detail.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Entry } from '../entry';
+import { EntryService } from '../entry.service';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+@Component({
+  selector: 'app-entries-detail',
+  templateUrl: './entries-detail.component.html',
+  styleUrls: ['./entries-detail.component.scss']
+})
+export class EntriesDetailComponent implements OnInit {
+  entry: Entry | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private entryService: EntryService,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.getEntry();
+  }
+
+  getEntry(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.entryService.getEntry(id)
+      .subscribe(entry => this.entry = entry);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+}
