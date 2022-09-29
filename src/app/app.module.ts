f6a1a3acbc202cb8d536592341c5b6784cb0d88d
Garrett Hays <103149986+GarrettHays@users.noreply.github.com>
2022-09-28 19:23:28 -0700

    adds app module typescript file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { EntriesComponent } from './entries/entries.component';
+import { EntriesDetailComponent } from './entries-detail/entries-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
+import { RecentComponent } from './recent/recent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EntriesComponent,
+    EntriesDetailComponent,
+    MessagesComponent,
+    RecentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
