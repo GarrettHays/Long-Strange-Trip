41f9f6da6f2a61139b5988421e19b67b126f868e
Garrett Hays <103149986+GarrettHays@users.noreply.github.com>
2022-09-28 19:23:09 -0700

    adds app component files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-long-strange-trip';
+  title = 'Long Strange Trip';
 }
